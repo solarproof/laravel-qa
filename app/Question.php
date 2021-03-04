6156e6c5fa45dea874ae24bd45d2530a779968c3
Chris Taeni <chris@solarproof.com.au>
2021-03-04 12:23:36 +1100

    Add votes, answers, and views counter to question item

--- app/Question.php
+++ app/Question.php
@@ -23,4 +23,15 @@ class Question extends Model
     public function getCreatedDateAttribute() {
         return $this->created_at->diffForHumans();
     }
+    public function getStatusAttribute() {
+        if ($this->answers > 0) {
+            if ($this->best_answer_id) {
+                return "answer-accepted";
+            }
+            return "answered";
+        }
+        else{
+            return "unanswered";
+        }
+    }
 }
