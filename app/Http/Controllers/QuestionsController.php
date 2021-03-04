605fbcd327775afd2c804c737afdc9fd188e8947
Chris Taeni <chris@solarproof.com.au>
2021-03-04 14:29:17 +1100

    Saving questions to the database and displaying a message

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AskQuestionRequest;
 use App\Question;
 use Illuminate\Http\Request;
 
@@ -39,9 +40,11 @@ class QuestionsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(AskQuestionRequest $request)
     {
-        //
+        $request->user()->questions()->create($request->all());
+
+        return redirect()->route('questions.index')->with('success', "Your question has been submitted");
     }
 
     /**
