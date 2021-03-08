57504350873920b85db3c3021d6c68631bbb127a
Chris Taeni <chris@solarproof.com.au>
2021-03-08 13:12:25 +1100

    Make model and migrations for answers

--- app/Question.php
+++ app/Question.php
@@ -24,7 +24,7 @@ class Question extends Model
         return $this->created_at->diffForHumans();
     }
     public function getStatusAttribute() {
-        if ($this->answers > 0) {
+        if ($this->answers_count > 0) {
             if ($this->best_answer_id) {
                 return "answer-accepted";
             }
@@ -38,5 +38,11 @@ class Question extends Model
     public function getBodyHtmlAttribute()
     {
         return \Parsedown::instance()->text($this->body);
+    }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+        // $question->answers()->count()
     }
 }
